8e87514eec4a24c47f7244a4d3d372669bece921
VladDulger <diversionerios@gmail.com>
2023-11-04 22:43:34 +0600

    Work Complite
    ConsoleAppEntityPostgreSQL

---
+++ ConsoleAppEntityPostgreSQL/Car.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEntityPostgreSQL
+{
+    internal class Car
+    {
+        public int Id { get; set; }
+
+        //Конвертирует Enum в строковое значение, когда сохраняет в json, а не int формат.
+        [JsonConverter(typeof(StringEnumConverter))]
+        public EnumCarsBrands Brand { get; set; }
+
+        public string Model { get; set; }
+    }
+}
---
+++ ConsoleAppEntityPostgreSQL/JsonFileManager.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEntityPostgreSQL
+{   
+    public static class JsonFileManager
+    {
+        internal static void SaveToJson(List<Car> cars, string fileName)
+        {
+            var json = JsonConvert.SerializeObject(cars, Formatting.Indented);
+            File.WriteAllText(fileName, json);
+        }
+
+        internal static List<Car> LoadFromJson(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                var json = File.ReadAllText(fileName);
+                return JsonConvert.DeserializeObject<List<Car>>(json);
+            }
+            return new List<Car>();
+        }
+    }
+}
